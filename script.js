d5400367ff1c0bd27ddaad5cf96f678c383719ab
MarlonDiniz <marlonsk45@gmail.com>
2022-07-25 12:59:51 -0300

    lista 2.0

--- script.js
+++ script.js
@@ -1,28 +1,69 @@
-function adicionar(){
-    document.getElementById("texto").value
-    document.getElementById("lista").innerHTML;
+const elementoLista = document.querySelector('ul')
+const elementoInput = document.querySelector('input')
+const elementoBotao = document.querySelector('button')
+
+
+
+const tarefas = JSON.parse(localStorage.getItem('list_tarefas'))??[]
+
+
+JSON.parse(localStorage.getItem('list_tarefas'))
+function mostraTarefas(){
+    
+    elementoLista.innerHTML = ''
+    
+    for (Item of tarefas){
+        
+        const elementoTarefa = document.createElement('li')
+        const textoTarefa = document.createTextNode(Item)
+        
+        const elementoLink = document.createElement('a')
+        const pos = tarefas.indexOf(Item)
+        
+        const linkText = document.createTextNode('Excluir Item')
+        elementoLink.appendChild(linkText)
+        
+        elementoLink.setAttribute('onclick', `deletaTarefa(${pos})`)
+        
+        elementoTarefa.appendChild(textoTarefa)
+        elementoLista.appendChild(elementoTarefa)
+        elementoTarefa.appendChild(elementoLink)
+    }
+}
+
+mostraTarefas()
+
+function addTarefa(){
 
     if (document.getElementById('texto').value.trim()==''){
-        alert('Preencha o campo, otário!')
+        alert('Preencha o campo, troxão!')
         document.getElementById('texto').focus()
         return false
-    }
-    
-    var text = document.getElementById("texto").value;
-    var list = document.getElementById("lista").innerHTML;
-    
-    list += "<li>" +text+ "</li>"
-    
-    document.getElementById("lista").innerHTML = list;
-    
-    document.getElementById("texto").value = null
-    document.getElementById('texto').focus()
-
-    
-    
+     }
+    const textoTarefa = elementoInput.value
+    tarefas.push(textoTarefa)
+    elementoInput.value = ''
+    salvarnolocalstorage()
+    mostraTarefas()
 }
-//não permitir adicionar campo vazio
+
+elementoBotao.setAttribute('onclick', 'addTarefa()')
+
+function deletaTarefa(pos){
+    tarefas.splice(pos, 1)
+    mostraTarefas()
+    salvarnolocalstorage()
+}
+
+function salvarnolocalstorage(){
+    localStorage.setItem('list_tarefas', JSON.stringify(tarefas))
+}
+
+
+
+//limitador de campo
 //permitir marcar o item
-//excluir item
 //editar item
 //adicionar quantidade
+//excluir item '-'
+//não permitir adicionar campo vazio '-'
